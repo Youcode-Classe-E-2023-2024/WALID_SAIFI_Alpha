70cbc4087e4376684425fb23080e2d02705d3c59
Walid Saifi <saifiwalid21@gmail.com>
2024-01-05 17:03:10 +0100

    Update login_controller.php

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,2 +1,18 @@
 <?php
-echo "valide";
+// session_start();
+// echo $_POST['log_email'];
+if (isset($_POST['log_email'])) {
+    $email = $_POST['email'];
+    $pasword = $_POST['password'];
+    $valide = user::login($pasword,$email);
+
+    if (is_int($valide) === true) {
+         header('location: index.php?page=dashbord');
+        echo 'valide';
+        exit();
+    } else {
+        header('location: index.php?page=login');
+        echo 'not valide';
+        exit();
+    }
+}
