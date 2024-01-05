ad43c7b82e772514f4bcde37c8b310aedb7a9615
Walid Saifi <saifiwalid21@gmail.com>
2024-01-05 12:03:24 +0100

    Update register_controller.php

---
+++ controllers/register_controller.php
@@ -0,0 +1,12 @@
+<?php
+ dd($_POST);
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+
+    user::registre($username,$password,$email);
+    header("location:index?page=login");
+
+}
