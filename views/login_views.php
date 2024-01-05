a44d47f8ec189882fa0ea5ace6997b1afafe73ec
Walid Saifi <saifiwalid21@gmail.com>
2024-01-05 16:05:03 +0100

    Update login_views.php

--- views/login_views.php
+++ views/login_views.php
@@ -1,7 +1,7 @@
 <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="bg-white p-8 shadow-md rounded-md w-full max-w-md bg-indigo-300">
         <h2 class="text-3xl font-bold text-center mb-6">Login</h2>
-        <form id="loginForm" action="index.php?page=login" method="post">
+        <form id="loginForm">
             <div class="mb-4">
                 <label for="email" class="block text-gray-700 text-sm font-semibold mb-2">Email</label>
                 <input type="email" name="email" id="email" placeholder="Enter your email" required
@@ -12,7 +12,7 @@
                 <input type="password" name="password" id="password" placeholder="Enter your password" required
                        class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
             </div>
-            <button type="submit"
+            <button id="submitBtn" name="submit_l"
                     class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline">
                 Login
             </button>
