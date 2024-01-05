0846016a5c88180d2fb4cf3d2ce3f55bc310f2a3
Walid Saifi <saifiwalid21@gmail.com>
2024-01-05 12:03:28 +0100

    Update Recovery_views.php

--- views/Recovery_views.php
+++ views/Recovery_views.php
@@ -1,3 +1,4 @@
+
 <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="bg-white p-8 shadow-md rounded-md w-full max-w-md bg-indigo-300">
         <h2 class="text-3xl font-bold text-center mb-6">Password Recovery</h2>
