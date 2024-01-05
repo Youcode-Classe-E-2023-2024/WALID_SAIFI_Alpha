9b4f71b101fc5751635f4d421ad25c0c97a6b019
Walid Saifi <saifiwalid21@gmail.com>
2024-01-05 12:08:46 +0100

    Update register_views.php

--- views/register_views.php
+++ views/register_views.php
@@ -17,7 +17,7 @@
                 <input type="password" name="password" id="password" placeholder="Enter your password" required
                        class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
             </div>
-            <button type="submit"
+            <button type="submit" name="submit"
                     class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline">
                 Register
             </button>
