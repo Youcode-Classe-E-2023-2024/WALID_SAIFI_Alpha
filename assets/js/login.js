d3075059ea98ac0c87f4ba2b330fa5875184aa7d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-08 09:19:24 +0100

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,5 @@
 const loginform = document.getElementById("loginForm");
 console.log(loginform);
-
 loginform.addEventListener("submit", function (event) {
     event.preventDefault();
     const formData = new FormData(this);
@@ -12,14 +11,10 @@ loginform.addEventListener("submit", function (event) {
         .then(response => response.text())
         .then(data => {
             console.log(data);
-
             if (data.trim() === 'valide') {
                 console.log('Login successful');
-                // Redirect to the home page or perform other actions
-              //  window.location.href = "index.php?page=home";
             } else {
                 console.log('Login failed');
-                // Handle failed login (e.g., display an error message)
             }
         })
         .catch(error => {
